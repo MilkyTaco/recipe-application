20cfb166c481e7a6db929163740515ddf87eac9c
kharl112 <kharl.dms112@gmail.com>
2021-04-16 22:10:07 -0500

    class bookmark and recipe in migrations renamed

--- backend/database/migrations/2021_04_17_024612_create_bookmark_table.php
+++ backend/database/migrations/2021_04_17_024612_create_bookmark_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Bookmark extends Migration
+class CreateBookmarkTable extends Migration
 {
     /**
      * Run the migrations.
