cfbbe5a6aeef11feb0fbb5ebd4a67e2f185eae01
kharl112 <kharl.dms112@gmail.com>
2021-04-17 06:03:15 -0500

    user login request and controller configured

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -4,29 +4,56 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
+use Illuminate\Http\Request;
 use Throwable;
 
 class UserController extends Controller
 {
     public function store(UserRequest $request)
-    {   
+    {
         $request->validated();
         $req = json_decode(json_encode($request->input()));
 
         $user = new User();
         $user->name = $req->name;
         $user->email = $req->email;
-        
-        $req->password = Hash::make('secret');
-        $user->password = $req->password;
+        $user->password = Hash::make($req->password);
 
         try {
             $user->save();
-            return ["success" => ["message" => "user saved successfully!", "data" => $user]];
+            return [
+                "success" => [
+                    "message" => "user saved successfully!",
+                    "data" => $user->id
+                ]
+            ];
         } catch (Throwable $e) {
             return response(["errors" => ["message" => "unresolved"]], 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $req = json_decode(json_encode($request->input()));
+
+        $user = User::where('email', $req->email)
+            ->get()->first();
+
+        try {
+            if (Hash::check($request->password, $user->password)) {
+                return ["success" => [
+                    "message" => "user saved successfully!",
+                    "data" => [
+                        "name" => $user->name,
+                        "email" => $user->email,
+                        "id" => $user->id
+                    ]
+                ]];
+            }
+            return response(["errors" => ["message" => "invalid password"]], 500);
+        } catch (Throwable $e) {
+            return response(["errors" => ["message" => "uncaught error"]], 500);
         }
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 Route::prefix('user')->group(function (){
     Route::post('signup', [UserController::class, "store"]);
-    Route::get('show', [UserController::class, "show"]);
+    Route::post('login', [UserController::class, "login"]);
 });
