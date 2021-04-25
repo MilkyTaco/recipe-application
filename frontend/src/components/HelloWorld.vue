02c9e0dbef0b6bc2a2c68ece1bec4e743f0ff9ad
kharl112 <kharl.dms112@gmail.com>
2021-04-25 00:54:48 -0500

    removed fetch method in Helloworld

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -4,12 +4,6 @@ export default {
   props: {
     msg: String,
   },
-  created() {
-    fetch("http://localhost/api")
-      .then(data => data.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
-  },
 };
 </script>
 
