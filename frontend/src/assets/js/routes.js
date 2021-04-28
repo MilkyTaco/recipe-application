a04c46ba733becf507c975e70ea12b0f08ffb6f9
kharl112 <kharl.dms112@gmail.com>
2021-04-27 23:01:07 -0500

    routes reconfigured

--- frontend/src/assets/js/routes.js
+++ frontend/src/assets/js/routes.js
@@ -1,17 +1,13 @@
 import Home from "../../components/Home";
 import Login from "../../components/Login";
-import Register from "../../components/Register";
-import Recipes from "../../components/children/Recipes";
 
 export const routes = [
   { path: "/", component: Login },
-  { path: "/signup", component: Register },
   {
     path: "/home",
     component: Home,
-    children: [{ path: "/home/recipes", component: Recipes }],
     beforeEnter: (to, from, next) => {
-      return sessionStorage.getItem("token") ? next() : next({ path: "/" });
+      return sessionStorage.getItem("Authorization") ? next() : next({ path: "/" });
     },
   },
 ];
