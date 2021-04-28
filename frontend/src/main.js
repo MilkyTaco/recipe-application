3319f9291f77a95d6bc07e9997db1028fe38e0fd
kharl112 <kharl.dms112@gmail.com>
2021-04-27 23:01:41 -0500

    router configured in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { routes } from "./assets/js/routes";
+import VueRouter from "vue-router";
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes,
+  mode: "history",
+});
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
