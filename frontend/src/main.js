ab155787d7984d5e9e12c5ad669d25d61de2d7b5
kharl112 <kharl.dms112@gmail.com>
2021-06-18 05:37:40 -0500

    router exported from main

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,10 +7,12 @@ import VueRouter from "vue-router";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
-const router = new VueRouter({
+
+export const router = new VueRouter({
   routes,
   mode: "history",
 });
+
 new Vue({
   vuetify,
   router,
