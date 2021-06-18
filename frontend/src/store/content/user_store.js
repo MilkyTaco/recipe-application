f60dddf54b5d2493f82108103086f1f73b35749c
kharl112 <kharl.dms112@gmail.com>
2021-06-18 05:55:54 -0500

    user_store created

---
+++ frontend/src/store/content/user_store.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { router } from "../../main";
+
+export default user = {
+  namespaced: true,
+  state: () => ({
+    profile: null,
+    loading: {
+      profile: false,
+      login: false,
+    },
+  }),
+  getters: {
+    getLoading: (state) => state.loading,
+    getProfile: (state) => state.profile,
+  },
+  mutations: {
+    setLoading: (state, { type, loading }) => (state.loading[type] = loading),
+    setProfile: (state, profile) => (state.profile = profile),
+  },
+  actions: {
+    Login: async ({ commit, dispatch }, form) => {
+      dispatch("message/defaultState", null, { root: true });
+      commit("setLoading", { loading: true, type: "login" });
+      try {
+        const { data } = await axios.post("/api/user/login", form);
+        sessionStorage.setItem("Authorization", `Bearer  ${data.token}`);
+        commit("setLoading", { loading: false, type: "login" });
+        return router.push("/home");
+      } catch (error) {
+        const { message } = error.response.data || error;
+        commit("setLoading", { loading: false, type: "login" });
+        return commit("message/setError", message, { root: true });
+      }
+    },
+  },
+};
