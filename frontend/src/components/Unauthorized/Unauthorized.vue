ddb1abf722d2f2afa3e61ee3f308304db9ed9f26
kharl112 <kharl.dms112@gmail.com>
2021-06-18 06:51:05 -0500

    Unauthorized parent component created

---
+++ frontend/src/components/Unauthorized/Unauthorized.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "Unauthorized",
+};
+</script>
+<template>
+  <v-container fluid class="pa-0 ma-0">
+    <v-app-bar color="black"> </v-app-bar>
+    <v-container fluid class="pa-0 ma-0">
+      <router-view></router-view>
+    </v-container>
+  </v-container>
+</template>
