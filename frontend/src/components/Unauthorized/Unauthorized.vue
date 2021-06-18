4614be06860cfae6c6b0f291eb91dedf81da7546
kharl112 <kharl.dms112@gmail.com>
2021-06-18 07:15:36 -0500

    Unauthorized component design fixed

--- frontend/src/components/Unauthorized/Unauthorized.vue
+++ frontend/src/components/Unauthorized/Unauthorized.vue
@@ -5,8 +5,41 @@ export default {
 </script>
 <template>
   <v-container fluid class="pa-0 ma-0">
-    <v-app-bar color="black"> </v-app-bar>
-    <v-container fluid class="pa-0 ma-0">
+    <v-app-bar color="black">
+      <v-container fluid class="pa-1">
+        <v-row justify="center" align="center">
+          <v-col cols="12" sm="6" md="5" align="center">
+            <v-row justify="space-between" align="center">
+              <v-col cols="3">
+                <router-link
+                  to="/login"
+                  class="white--text text-decoration-none"
+                >
+                  LOGIN
+                </router-link>
+              </v-col>
+              <v-col cols="5">
+                <v-img
+                  src="../../assets/images/logo.png"
+                  max-width="200"
+                  class="mb-n10 mb-md-n16 mb-sm-n14 rounded-circle"
+                  elevation="5"
+                />
+              </v-col>
+              <v-col cols="3">
+                <router-link
+                  to="/signup"
+                  class="white--text text-decoration-none"
+                >
+                  SIGNUP
+                </router-link>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+    <v-container fluid class="pa-0 ma-0 mt-10">
       <router-view></router-view>
     </v-container>
   </v-container>
