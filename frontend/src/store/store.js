6a2fdb9e8e0dc6db82e00df4b19be4a54c6d25d7
kharl112 <kharl.dms112@gmail.com>
2021-06-18 05:58:19 -0500

    message and user store imported in store.js

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,13 @@
 import Vuex from "vuex";
 import Vue from "vue";
+import message from "./content/message_store";
+import user from "./content/user_store";
 
-const store = new Vuex.store({});
+Vue.use(Vuex);
+
+const store = new Vuex.store({
+  message,
+  user,
+});
 
 export default store;
