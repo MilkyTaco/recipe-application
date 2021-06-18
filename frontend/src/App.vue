e10405fecd00e552f5b125743732d877034e986f
kharl112 <kharl.dms112@gmail.com>
2021-06-18 06:56:14 -0500

    margin and padding removed from App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,7 @@ export default {
 <template>
   <v-app>
     <v-main class="main-container">
-      <v-container fluid class="sub-container">
+      <v-container fluid class="sub-container pa-0 ma-0">
         <router-view> </router-view>
       </v-container>
     </v-main>
