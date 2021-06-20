3535c8af292ef44de6051a258800100f2e970ce8
kharl112 <kharl.dms112@gmail.com>
2021-06-20 03:29:53 -0500

    Register component fixed

--- frontend/src/components/Unauthorized/content/Register.vue
+++ frontend/src/components/Unauthorized/content/Register.vue
@@ -2,12 +2,29 @@
 export default {
   name: "Register",
   data: () => ({
+    rules: [(e) => !!e || "this field is required"],
     form: {
       name: "",
       email: "",
       password: "",
     },
   }),
+  computed: {
+    getSignupError() {
+      return this.$store.getters["message/getError"];
+    },
+    getSignupLoading() {
+      return this.$store.getters["user/getLoading"].signup;
+    },
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      if (this.$refs.form.validate()) {
+        this.$store.dispatch("user/Signup", this.form);
+      }
+    },
+  },
 };
 </script>
 <template>
@@ -15,7 +32,7 @@ export default {
     <v-row justify="center" align="center">
       <v-col col="10" sm="6" md="3">
         <v-card dark class="ma-2 pa-5 pt-7 pb-7 rounded-xl">
-          <v-form ref="form">
+          <v-form ref="form" @submit="handleSubmit">
             <v-row justify="center" align="start">
               <v-col cols="12">
                 <v-row justify="center">
@@ -28,9 +45,11 @@ export default {
                 <v-text-field
                   rounded
                   outlined
+                  :disabled="getSignupLoading"
                   autofocus
+                  :rules="rules"
                   append-icon="mdi-text"
-                  v-model="form.email"
+                  v-model="form.name"
                   label="Name"
                   type="text"
                 />
@@ -39,7 +58,8 @@ export default {
                 <v-text-field
                   rounded
                   outlined
-                  autofocus
+                  :disabled="getSignupLoading"
+                  :rules="rules"
                   append-icon="mdi-email"
                   v-model="form.email"
                   label="email"
@@ -50,19 +70,24 @@ export default {
                 <v-text-field
                   rounded
                   outlined
+                  :disabled="getSignupLoading"
+                  :rules="rules"
                   append-icon="mdi-lock"
                   v-model="form.password"
                   label="password"
                   type="password"
                 />
               </v-col>
-              <v-col v-if="false" class="pa-1">
-                <v-alert class="alert pt-2 pb-2" type="error"> </v-alert>
+              <v-col v-if="getSignupError" class="pa-1">
+                <v-alert class="alert pt-2 pb-2" type="error">
+                  {{ getSignupError }}
+                </v-alert>
               </v-col>
               <v-col cols="12" class="pa-1">
                 <v-btn
                   type="submit"
                   rounded
+                  :loading="getSignupLoading"
                   block
                   elevation="0"
                   color="warning"
