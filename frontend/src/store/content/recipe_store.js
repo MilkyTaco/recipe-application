71514bd4a29a108ffa6eceee2af5c2aebe115e3c
kharl112 <kharl.dms112@gmail.com>
2021-07-08 02:13:14 -0500

    Recipe action and other methods created in recipe_store

--- frontend/src/store/content/recipe_store.js
+++ frontend/src/store/content/recipe_store.js
@@ -5,16 +5,20 @@ const recipe = {
   namespaced: true,
   state: () => ({
     recipes: [],
+    recipe: null,
     loading: {
       recipes: false,
+      recipe: false,
     },
   }),
   getters: {
     getAllRecipes: (state) => state.recipes,
+    getRecipe: (state) => state.recipe,
     getLoading: (state) => state.loading,
   },
   mutations: {
     setAllRecipes: (state, recipes) => (state.recipes = [...recipes]),
+    setRecipe: (state, recipe) => (state.recipe = { ...recipe }),
     setLoading: (state, { type, loading }) => (state[type].loading = loading),
   },
   actions: {
@@ -30,6 +34,26 @@ const recipe = {
       } catch (error) {
         const message = error.response.data.errors || error;
         commit("setLoading", { loading: false, type: "recipes" });
+        return commit(
+          "message/setError",
+          JSON.stringify(message).replace(/\W/gi, " "),
+          {
+            root: true,
+          }
+        );
+      }
+    },
+    RecipeInfo: async ({ commit }) => {
+      commit("setLoading", { loading: true, type: "recipe" });
+      try {
+        const { data } = await axios.get(`${api}/recipe/view/${id}`, {
+          headers: { Authorization: sessionStorage.getItem("Authorization") },
+        });
+        commit("setLoading", { loading: false, type: "recipe" });
+        return commit("setRecipe", data);
+      } catch (error) {
+        const message = error.response.data.errors || error;
+        commit("setLoading", { loading: false, type: "recipe" });
         return commit(
           "message/setError",
           JSON.stringify(message).replace(/\W/gi, " "),
