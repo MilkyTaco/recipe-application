4782b005d8a88aa7d84833ded7e46050c1fdf69b
kharl112 <kharl.dms112@gmail.com>
2021-07-07 23:43:39 -0500

    recipe_store created

---
+++ frontend/src/store/content/recipe_store.js
@@ -0,0 +1,40 @@
+const recipe = {
+  usespaced: true,
+  state: () => ({
+    recipes: [],
+    loading: {
+      recipes: false,
+    },
+  }),
+  getters: {
+    getAllRecipes: (state) => state.recipes,
+    getLoading: (state) => state.loading,
+  },
+  mutations: {
+    setAllRecipes: (state, recipes) => (state.recipes = [...recipes]),
+    setLoading: (state, { type, loading }) => (state[type].loading = loading),
+  },
+  actions: {
+    AllRecipes: ({ commit, state }) => {
+      if (!state.recipes[0])
+        commit("setLoading", {loading: true , type: "recipes"});
+      try {
+        const { data } = await axios.get(`${api}/recipe/show/all`);
+        commit("setLoading", { loading: false, type: "recipes" });
+        return commit("setAllRecipes", data);
+      } catch (error) {
+        const message = error.response.data.errors || error;
+        commit("setLoading", { loading: false, type: "recipes" });
+        return commit(
+          "message/setError",
+          JSON.stringify(message).replace(/\W/gi, " "),
+          {
+            root: true,
+          }
+        );
+      }
+    },
+  },
+};
+
+export default recipe;
