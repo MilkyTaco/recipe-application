942f963d5cd1e203c27c1d31a5a12a6d459e023a
kharl112 <kharl.dms112@gmail.com>
2021-07-11 02:53:48 -0500

    Account component data fetch fixed

--- frontend/src/components/Home/content/Account.vue
+++ frontend/src/components/Home/content/Account.vue
@@ -1,11 +1,90 @@
 <script>
 export default {
   name: "Account",
+  data: () => ({
+    form: null,
+    rules: [(e) => !!e || "this field is required"],
+  }),
+  computed: {
+    getUserLoading() {
+      return this.$store.getters["user/getLoading"];
+    },
+    user() {
+      return this.$store.getters["user/getProfile"];
+    },
+  },
+  created() {
+    this.form = JSON.parse(JSON.stringify(this.user));
+  },
 };
 </script>
 <template>
-  <v-container fluid class="mt-10">
-    <v-row justify="center"> </v-row>
+  <v-container fluid>
+    <v-row justify="center" v-if="!getUserLoading.profile">
+      <v-col cols="12" md="7">
+        <v-sheet
+          class="pa-7 pa-sm-12 pa-md-12 ma-2 mt-10"
+          elevation="2"
+          color="grey lighten-3"
+        >
+          <v-row justify="start">
+            <v-col cols="12">
+              <v-form ref="form">
+                <v-row justify="start">
+                  <v-col cols="12" align="start">
+                    <span>Account Settings </span>
+                  </v-col>
+                  <v-col md="4" sm="5" align="start">
+                    <v-text-field
+                      outlined
+                      dense
+                      v-model="form.id"
+                      label="ID"
+                      :rules="rules"
+                      disabled
+                    />
+                  </v-col>
+                  <v-col md="8" sm="7" align="start">
+                    <v-text-field
+                      outlined
+                      dense
+                      v-model="form.name"
+                      label="Name"
+                      :rules="rules"
+                    />
+                  </v-col>
+                  <v-col cols="12" class="pt-0">
+                    <v-text-field
+                      outlined
+                      dense
+                      v-model="form.email"
+                      label="Email"
+                      :rules="rules"
+                    />
+                  </v-col>
+                  <v-col cols="12" class="pt-0">
+                    <v-btn color="primary" max-width="200" min-width="120">
+                      <v-icon left> mdi-content-save </v-icon>
+                      Save
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-form>
+            </v-col>
+          </v-row>
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-row justify="start" v-else>
+      <v-col cols="12" md="7">
+        <v-skeleton-loader
+          class="mx-auto"
+          max-width="300"
+          min-width="270"
+          type="article, article, actions"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
