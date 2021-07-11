e43a89eb4638badb5ad991e852a38eb1df1fd1fa
kharl112 <kharl.dms112@gmail.com>
2021-07-11 02:27:10 -0500

    Account component created

---
+++ frontend/src/components/Home/content/Account.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: "Account",
+};
+</script>
+<template>
+  <v-container fluid class="mt-10">
+    <v-row justify="center"> </v-row>
+  </v-container>
+</template>
+
+<style scoped></style>
